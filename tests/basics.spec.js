48727bc41b2d11bc0ca048dc6662494da7dd3691
mdmehdi0099 <mdmehdi0099@gmail.com>
2026-09-21 21:53:52 +0530

    updating the test2 changes

--- tests/basics.spec.js
+++ tests/basics.spec.js
@@ -1,6 +1,21 @@
 import { test } from '@playwright/test';
 
-test('login',async function testPlaywright({browser}){
+test('login',async({browser})=>{
+//fixture: Ready-to-use setup provided by playwright for a test
+//browser
+//context
+//page
+const context=await browser.newContext();
+const page=await context.newPage();
+await page.goto("https://automationpracticehub.com/");
+//username
+//password
+//login button click
+await page.waitForTimeout(5000);
+
+});
+
+test('signIn',async({browser})=>{
 //fixture: Ready-to-use setup provided by playwright for a test
 //browser
 //context

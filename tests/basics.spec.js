685b62673fc41cb3cd2a53abf07e53d9d460296d
mdmehdi0099 <mdmehdi0099@gmail.com>
2026-09-21 21:43:47 +0530

    uploading the initial changes

---
+++ tests/basics.spec.js
@@ -0,0 +1,16 @@
+import { test } from '@playwright/test';
+
+test('login',async function testPlaywright({browser}){
+//fixture: Ready-to-use setup provided by playwright for a test
+//browser
+//context
+//page
+const context=await browser.newContext();
+const page=await context.newPage();
+await page.goto("https://automationpracticehub.com/");
+//username
+//password
+//login button click
+await page.waitForTimeout(5000);
+
+});
